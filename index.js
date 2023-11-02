b307251e3065e287a2160705ab0d0bc8d4943855
Phantom-2 <100276440+Phantom-2@users.noreply.github.com>
2023-11-02 12:48:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,259 @@
 <!DOCTYPE html>
-<html>
+<html lang="ar">
 <head>
-    <title>IP Checker</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>طوفان مصر</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: right;
+            margin: 20px;
+        }
+        h1 {
+            text-align: center;
+        }
+        p {
+            font-size: 18px;
+            line-height: 1.5;
+        }
+        .list-indent {
+            margin-right: 40px; /* Adjust the value as needed for your desired indentation */
+        }
+    </style>
 </head>
 <body>
-    <h1>IP Checker</h1>
-    <p id="result"></p>
+    <h1>بسم الله الرحمن الرحيم</h1>
+    
+    <section>
+        <h2>مقدمة</h2>
+        <p dir="rtl"><span>على الرغم من ان السيسى قام بالتالى:</span></p>
+        <ul>
+        <li dir="rtl" class="list-indent"><p dir="rtl"><span class="C9DxTc " style="font-variant: normal;">انقلب على الرئيس محمد مرسى وقتله بدم بارد.</span></p></li>
+        <li dir="rtl" class="list-indent"><p dir="rtl"><span class="C9DxTc " style="font-variant: normal;"> قتل واعتقل آلاف المتظاهرين.</span></p></li>
+        <li dir="rtl" class="list-indent"><p dir="rtl"><span class="C9DxTc " style="font-variant: normal;"> تولى مصر والدين الخارجى ٤٦ مليار, الدين الخارجى الان&nbsp; ١٦٤.٧ مليار</span></p></li>
+        <li dir="rtl" class="list-indent"><p dir="rtl"><span class="C9DxTc " style="font-variant: normal;"> باع تيران وصنافير للسعوديه</span></p></li>
+        <li dir="rtl" class="list-indent"><p dir="rtl"><span class="C9DxTc " style="font-variant: normal;"> تنازل عن حق مصر فى مياه النيل.</span></p></li>
+        <li dir="rtl" class="list-indent"><p dir="rtl"><span class="C9DxTc " style="font-variant: normal;"> باع ومازال يبيع اصول مصر لسداد الديون.</span></p></li>
+        <li dir="rtl" class="list-indent"><p dir="rtl"><span class="C9DxTc " style="font-variant: normal;"> حكم مصر من ٢٠١٤ الى ٢٠١٧ ثم من ٢٠١٨ الى ٢٠٢١ ثم قام بتعديل الدستور واضاف سنتين وبعد هذا كله يريد الترشح لفترة ثالثه ويرفض اجراء انتخابات نزيهه&nbsp; وراينا ما حدث مع احمد طنطاوى</span></p></li>
+        </ul>
+        <br><p dir="rtl"><span>الا اننا كنا على اتم الاستعداد للوقوف معه فى القضية الفلسطينيه, وحتى الذين ظلمهم فى السجون قالو “احنا لا يعنينا ان تحكم علينا باحكام اعدام وسجون فلسطين قضيتنا الاولى”&nbsp;</span>
+        <br>
+        <br>
+        <br>
+    </section>
 
-    <script>
-        // Get query parameters from the URL
-        const urlParams = new URLSearchParams(window.location.search);
-        const ip = urlParams.get('ip');
+    <section>
+        <h2>ولكن</h2>
+        <p dir="rtl"><span>دعمت القوى الغربية المحتل بالسلاح فبدلا من ان&nbsp; يمثل دور مصر كاقوى جيش عربى ويسارع الى انشاء تحالف اسلامى مكافىء للقوى الغربية التى اُنهكت بالحرب ضد روسيا اكتفى بالمناشدة والمطالبه وليس هذا فحسب بل اغلق معبر رفح بعد ان فرض المحتل حصاره الغاشم فاصبح شريك معهم فى الحصار, كما نقلت أسوشيتد برس عن مسؤول في المخابرات المصرية إن مصر "تحدثت مرارا وتكرارا مع الإسرائيليين عن شيء كبير سيحدث”.</span></p>
+        <p dir="rtl"><span>نحن امام خيانة مكتملة الاركان</span></p>
+        <br>
+        <br>
+        <br>
+    </section>
 
-        // Send a GET request to the external server
-        fetch(`https://server2.com/ip?ip=${ip}`)
-            .then(response => {
-                if (response.status === 200) {
-                    document.getElementById('result').textContent = 'Good IP';
-                } else {
-                    document.getElementById('result').textContent = 'Wrong IP';
-                }
-            })
-            .catch(error => {
-                console.error(error);
-                document.getElementById('result').textContent = 'Error';
-            });
-    </script>
+    <section>
+        <h2>وعليه</h2>
+        <p dir="rtl"><span>هذا الرجل يجب ان يرحل فورا هو ومن شارك معه فى الخيانة,&nbsp; كفى ١٠ سنين فشل اقتصاديا وسياسيا وانسانيا وعسكريا + خيانه + جبن(تم ضرب معبر رفح ٤ مرات) واخيراً نأخذ الاذن من اسرائيل لادخال المساعدات!! الكيل طفح. تجاهل الامر كمصاب السرطان يتجاهل العلاج فينتشر السرطان فى كامل جسده, فكل اجراء يتخذه هذا الشخص كإتفاقية المبادىء لا تُأثر علينا فحسب بل ستُاثر على اجيال ستلعنه وتلعننا معه لسكوتنا عليه.</span></p>
+        <p dir="rtl"><span>كيف لشخص واحد او حتى ١٠٠٠ يتحكمو فى مصير ملايين!!&nbsp;</span></p> 
+        <br>
+        <br>
+        <br>
+    </section>
+
+    <section>
+        <h2>كيف؟؟</h2>
+        <p dir="rtl"><span>الحل بسيط وهو الاتحاد</span></p>
+        <p dir="rtl"><span>تخيل ان مدير مستبد يسىء معاملة الموظفين ويهددهم بالفصل&nbsp;</span></p>
+        <p dir="rtl"><span>لو كل موظف فكر فى نفسه فقط وانه قد يفصل ان تحدى المدير سيبقى الوضع كما هو</span></p>
+        <p dir="rtl"><span>ولكن لو اتحد الموظفون وتحدو المدير لن يستطيع ان يرفدهم جميعا وسيتغير الوضع</span></p>
+        <p dir="rtl"><span>فكرة ان مجموعه تنزل للتظاهر ومجموعة تجلس كمحلليين تقول “المفروض يعملو … والمفروض ميعملوش …” مرفوض شكلا وموضوعا</span></p>
+        <p dir="rtl"><span>الجميع يجب ان يشارك ولن نتظاهر&nbsp;</span></p>
+        <p dir="rtl"><span>فهذا الشخص اعتاد منذ ٢٠١٣ مظاهرات = قمع</span></p>
+        <br><p dir="rtl"><span>نحن سنقوم باضراب عام, الجميع الجميع الجميع سيجلس فى البيت, فلنتكاتف جميعا ونتحد ويعاون بعضنا بعض حتى يرحل حتى يرحل ذلك الشخص, سنغلق مصر وسيكون امامه خيارين:</span></p>
+        <ul class="n8H08c UVNKR " style="list-style-type: square; margin-left: 0; margin-right: 0; padding: 0;"><li dir="rtl" class="zfr3Q TYR86d eD0Rn " style="margin-right: 15.0pt;"><p dir="rtl" role="presentation" class="zfr3Q CDt4Ke " style="margin-right: 0.0pt; padding-right: 0.0pt; text-indent: 0.0pt;"><span>اما ان يكون متبقى عنده ذرة حب للبلد فسيرى ان جميع الشعب لا يريده وان البلد ستذهب الى وضع حرج ان استمر الحال ومن ثم ياخذ بعضه ويذهب الى بلد اخر.</span></p>
+        </li><li dir="rtl" class="zfr3Q TYR86d eD0Rn " style="margin-right: 15.0pt;"><p dir="rtl" role="presentation" class="zfr3Q CDt4Ke " style="margin-right: 0.0pt; padding-right: 0.0pt; text-indent: 0.0pt;"><span>واما ان يبقى ويتجاهل وضع البلد عندها المقربين منه&nbsp; سيضعونه فى اقرب طائرة ويقولون له “الناس لا تريدك والبلد فى وضع حرج, انت لن تحكم الناس رغما عنهم”</span></p>
+        </li></ul><br><p dir="rtl"><span>ولكن حتى تنجح الخطه يحب ان يشارك الجميع فلذلك لن يتم البدأ فى الاضراب حتى يصل عدد المشاركين الى ١٠ مليون شخص</span></p>
+        <p dir="rtl"><span>ولنعلم انه لا يوجد شىء بالساهل, نريد حياة كريمة وحاكم صالح علينا ان نبادر ونتحرك لذلك</span></p>
+        <p dir="rtl"><span>"وما نيل المطالب بالتمني ولكن تؤخذ الدنيا غلابا"</span></p>
+        <br>
+        <br>
+        <br>
+    </section>
+
+    <section>
+        <h2>لذلك</h2>
+        <p>تم انشاء ٥٢ سيرفر ديسكورد يجب ان تمتلىء بالمشاركين وفى حال وصول إجمالى عدد المشاركين فى المبادرة الى ١٠ مليون شخص سنبدأ فى الاضراب.</p>
+        <p><u>: انضم الى احد السيرفرات التاليه بالضغط على الرابط</u></p>
+        <h4>القاهره</h4>
+        <ol>
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/TBeQkF2t" target="_blank" style="text-decoration: none;"><span>https://discord.gg/TBeQkF2t</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/TFEgpyVj" target="_blank" style="text-decoration: none;"><span>https://discord.gg/TFEgpyVj</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/HN6RdyXu" target="_blank" style="text-decoration: none;"><span>https://discord.gg/HN6RdyXu</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/7vDDEdAs" target="_blank" style="text-decoration: none;"><span>https://discord.gg/7vDDEdAs</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/GJv8VjWZ" target="_blank" style="text-decoration: none;"><span>https://discord.gg/GJv8VjWZ</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/jVsgNu45" target="_blank" style="text-decoration: none;"><span>https://discord.gg/jVsgNu45</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/GCpfTUz4" target="_blank" style="text-decoration: none;"><span>https://discord.gg/GCpfTUz4</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/wEzg5yAx" target="_blank" style="text-decoration: none;"><span>https://discord.gg/wEzg5yAx</span></a></li>
+        </ol>
+
+        <h4>الجيزه</h4>
+        <ol>
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/DysyM2tv" target="_blank" style="text-decoration: none;"><span>https://discord.gg/DysyM2tv</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/sFgdErhC" target="_blank" style="text-decoration: none;"><span>https://discord.gg/sFgdErhC</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/3n4ekMds" target="_blank" style="text-decoration: none;"><span>https://discord.gg/3n4ekMds</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/sxgMjvqr" target="_blank" style="text-decoration: none;"><span>https://discord.gg/sxgMjvqr</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/FwKfmAp5" target="_blank" style="text-decoration: none;"><span>https://discord.gg/FwKfmAp5</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/N4FYCc4K" target="_blank" style="text-decoration: none;"><span>https://discord.gg/N4FYCc4K</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/mjz8pePj" target="_blank" style="text-decoration: none;"><span>https://discord.gg/mjz8pePj</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/VbJAvKMU" target="_blank" style="text-decoration: none;"><span>https://discord.gg/VbJAvKMU</span></a></li>
+        </ol>
+
+        <h4>الشرقية</h4>
+        <ol>
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/Bj5PZVRV" target="_blank" style="text-decoration: none;"><span>https://discord.gg/Bj5PZVRV</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/eYqn6C8C" target="_blank" style="text-decoration: none;"><span>https://discord.gg/eYqn6C8C</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/aGSzdSFg" target="_blank" style="text-decoration: none;"><span>https://discord.gg/aGSzdSFg</span></a></li>
+        </ol>
+
+
+        <h4>الدقهلية</h4>
+        <ol>
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/yVtUYzc8" target="_blank" style="text-decoration: none;"><span>https://discord.gg/yVtUYzc8</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/PavmRwW8" target="_blank" style="text-decoration: none;"><span>https://discord.gg/PavmRwW8</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/SDkEb9G4" target="_blank" style="text-decoration: none;"><span>https://discord.gg/SDkEb9G4</span></a></li>
+        </ol>
+
+        <h4>البحيره ومطروح والوادى الجديد</h4>
+        <ol>
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/dNVJxx52" target="_blank" style="text-decoration: none;"><span>https://discord.gg/dNVJxx52</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/63VzVUyB" target="_blank" style="text-decoration: none;"><span>https://discord.gg/63VzVUyB</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/CkCHaDGb" target="_blank" style="text-decoration: none;"><span>https://discord.gg/CkCHaDGb</span></a></li>
+        </ol>
+
+         <h4>المنيا</h4>
+        <ol>
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/8cs46eu5" target="_blank" style="text-decoration: none;"><span>https://discord.gg/8cs46eu5</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/WuHx4G5e" target="_blank" style="text-decoration: none;"><span>https://discord.gg/WuHx4G5e</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/ErfanycD" target="_blank" style="text-decoration: none;"><span>https://discord.gg/ErfanycD</span></a></li>
+        </ol>
+
+        <h4>القليوبية</h4>
+        <ol>
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/wNSArS4Y" target="_blank" style="text-decoration: none;"><span>https://discord.gg/wNSArS4Y</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/aUuTcnmf" target="_blank" style="text-decoration: none;"><span>https://discord.gg/aUuTcnmf</span></a></li>
+          
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/HDCKPrbT" target="_blank" style="text-decoration: none;"><span>https://discord.gg/HDCKPrbT</span></a></li>
+        </ol>
+
+        <h4>سوهاج</h4>
+        <ol>
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/mR4KbjUY" target="_blank" style="text-decoration: none;"><span>https://discord.gg/mR4KbjUY</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/MgR3u3Mp" target="_blank" style="text-decoration: none;"><span>https://discord.gg/MgR3u3Mp</span></a></li>
+        </ol>
+
+        <h4>اسيوط</h4>
+        <ol>
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/GPqV3pWv" target="_blank" style="text-decoration: none;"><span>https://discord.gg/GPqV3pWv</span></a></li>
+    
+          <li dir="rtl" class="list-indent"><a href="https://discord.gg/JHXUBPTn" target="_blank" style="text-decoration: none;"><span>https://discord.gg/JHXUBPTn</span></a></li>
+        </ol>
+
+        <h4>المنوفية</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/9Pp2GdEw" target="_blank" style="text-decoration: none;"><span>https://discord.gg/9Pp2GdEw</span></a></li>
+    
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/btUtnAzC" target="_blank" style="text-decoration: none;"><span>https://discord.gg/btUtnAzC</span></a></li>
+        </ol>
+
+        <h4>الفيوم</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/3eQSYMAF" target="_blank" style="text-decoration: none;"><span>https://discord.gg/3eQSYMAF</span></a></li>
+
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/FqGp7F5t" target="_blank" style="text-decoration: none;"><span>https://discord.gg/FqGp7F5t</span></a></li>
+        </ol>
+
+        <h4>كفر الشيخ</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/7JhTvUkR" target="_blank" style="text-decoration: none;"><span>https://discord.gg/7JhTvUkR</span></a></li>
+        </ol>
+
+        <h4>بنى سويف</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/Se5Asrmh" target="_blank" style="text-decoration: none;"><span>https://discord.gg/Se5Asrmh</span></a></li>
+        </ol>
+
+        <h4>قنا</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/JQ4EebDR" target="_blank" style="text-decoration: none;"><span>https://discord.gg/JQ4EebDR</span></a></li>
+        </ol>
+
+        <h4>الاسكندريه</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/E4HKZvj8" target="_blank" style="text-decoration: none;"><span>https://discord.gg/E4HKZvj8</span></a></li>
+    
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/3W8FtX3q" target="_blank" style="text-decoration: none;"><span>https://discord.gg/3W8FtX3q</span></a></li>
+            
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/DyhsqGQD" target="_blank" style="text-decoration: none;"><span>https://discord.gg/DyhsqGQD</span></a></li>        </ol>
+
+        <h4>الغربية</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/aUR8pcuS" target="_blank" style="text-decoration: none;"><span>https://discord.gg/aUR8pcuS</span></a></li>
+    
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/qyJRhQ6t" target="_blank" style="text-decoration: none;"><span>https://discord.gg/qyJRhQ6t</span></a></li>        </ol>
+
+        <h4>دمياط و بورسعيد</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/CBWgkUFP" target="_blank" style="text-decoration: none;"><span>https://discord.gg/CBWgkUFP</span></a></li>
+        </ol>
+
+        <h4>الاقصر واسوان</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/GtzgrVgv" target="_blank" style="text-decoration: none;"><span>https://discord.gg/GtzgrVgv</span></a></li>
+        </ol>
+
+        <h4>الاسماعيلية والسويس</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/ak3S7TPd" target="_blank" style="text-decoration: none;"><span>https://discord.gg/ak3S7TPd</span></a></li>
+    
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/C7MszeEK" target="_blank" style="text-decoration: none;"><span>https://discord.gg/C7MszeEK</span></a></li>        </ol>
+        </ol>
+
+        <h4>البحر الاحمر وسيناء</h4>
+        <ol>
+            <li dir="rtl" class="list-indent"><a href="https://discord.gg/4cvyQutb" target="_blank" style="text-decoration: none;"><span>https://discord.gg/4cvyQutb</span></a></li>
+        </ol>
+        <br>
+        <br>
+        <br>
+    </section>
+
+    <section>
+        <p dir="rtl"><span>واخيرا فى حال تريد تعديل هذه الصفحة اضغط </span><a href="https://github.com/Phantom-2/tofanmisr/tree/main" target="_blank" style="color: inherit; text-decoration: none;"><span style="text-decoration: underline;">هنا</span></a></p>
+    </section>
 </body>
 </html>
